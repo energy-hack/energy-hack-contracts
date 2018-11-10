f029100229933b0607771a82f1c8610fe49de3f0
Dmitx <37021407+Dmitx@users.noreply.github.com>
2018-11-11 00:57:10 +0300

    Removed startKwh in constructor

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,7 +2,6 @@ var Token = artifacts.require("./SchneiderToken.sol");
 var SchneiderSys = artifacts.require("./SchneiderSystem.sol");
 
 const endTime =  Math.round((new Date().getTime() + 30000)/1000); // Now + 30 seconds;
-const startKwh = 30007873709;
 const prevPeriodKwh = 43368663;
 const goalPeriodKwh = 24368663;
 const customer = "0xB580f73DB43015A1211A394Eb175a81dF55bBe66";
@@ -13,7 +12,7 @@ module.exports = function(deployer) {
   deployer.deploy(Token)
   .then(function() {
       return deployer.deploy(SchneiderSys,
-        Token.address, endTime, startKwh, prevPeriodKwh, goalPeriodKwh, customer, schneider);
+        Token.address, endTime, prevPeriodKwh, goalPeriodKwh, customer, schneider);
   }).then(function() {
       return Token.deployed();
   }).then(function(InstanceToken) {
