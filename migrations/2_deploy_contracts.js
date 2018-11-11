fd14fc31af93956b426bea8fc6763a70456b5ab3
Dmitx <37021407+Dmitx@users.noreply.github.com>
2018-11-11 04:04:46 +0300

    Deleted unused const

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,7 +4,6 @@ var SchneiderSys = artifacts.require("./SchneiderSystem.sol");
 const endTime =  Math.round((new Date().getTime() + 15 * 60000)/1000); // Now + 15 minutes;
 const updateTime = 2 * 60; // 2 minutes
 const curLoad = 43368663;
-const goalPeriodKwh = 24368663;
 const customer = "0xB580f73DB43015A1211A394Eb175a81dF55bBe66";
 const schneider = "0x42072709803c2ee3EB16Ac7039429Bd63b031296";
 const mintValue = 1234567891234567800;
